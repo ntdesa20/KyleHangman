764c9cdcec68ec9e304acfce636d98f6e2f5285a
ntdesa20 <60932543+ntdesa20@users.noreply.github.com>
2020-03-02 11:14:50 -0500

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -35,3 +35,15 @@ def main():
     checkLetter()
 
 main()
+
+def checkLetter():
+    global updatedSpaces
+    if letter in word:
+        checkifWon()
+        updatedSpaces = updatedSpaces - 1
+        
+    else:
+        lives = lives - 1
+        getLetter() 
+        
+checkLetter()        
